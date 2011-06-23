bf633a89f95df979724098853e06c3f655dafd65
TandZ <simpletechsupportlex@gmail.com>
2011-06-23 13:03:44 -0400

    Fixed template local references

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATE_DIRS = (
 
-    '/home/zach/mysite/mysite/templates',
+    '/mysite/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
