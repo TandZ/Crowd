7feda7bf0e7b5430de4a50091f66054c38b6cdc2
TandZ <simpletechsupportlex@gmail.com>
2011-06-23 13:08:24 -0400

    deleted /mysite from template and database pathnames

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mysite/mydata',              # Or path to database file if using sqlite3.
+        'NAME': 'mydata',              # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATE_DIRS = (
 
-    '/mysite/templates',
+    '/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
